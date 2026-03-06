63730d213f317d84c6fd431923a4e6b450b7774d
anton_cooper <antncooper@gmail.com>
2026-03-06 12:11:26 -0800

    task two and three uncommented

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,16 @@
-// 'use strict';
+'use strict';
 
-// function makeArray(firstArray, secondArray, maxLength) {
-//   let combainedArray = firstArray.concat(secondArray);
-//   if (combainedArray.length > maxLength) {
-//     return combainedArray.slice(0, maxLength);
-//   } else {
-//     return combainedArray;
-//   }
-// }
-// console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
-// console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-// console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-// console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
+function makeArray(firstArray, secondArray, maxLength) {
+  let combainedArray = firstArray.concat(secondArray);
+  if (combainedArray.length > maxLength) {
+    return combainedArray.slice(0, maxLength);
+  } else {
+    return combainedArray;
+  }
+}
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
